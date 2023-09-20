ec6ac11ba161e4a48f968023c86756e3faa0fa1b
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-09-20 10:55:52 +0000

    Add file

---
+++ server.py
@@ -0,0 +1,53 @@
+import os
+
+import requests
+from flask import Flask, Response, request
+
+app = Flask(__name__)
+# Get BOT Token from telegram
+token = os.environ['TELEGRAM_BOT_TOKEN']
+
+
+# https://api.telegram.org/bot{token}/setWebhook?url=https://telebot-college-test.ridwaanhall.repl.co/
+def generate_answer(question):
+  url = 'https://earthquake-bmkg-api.ridwaanhall.repl.co/new.json'
+  response = requests.get(url)
+  print(response)
+  if response.status_code == 200:
+    return response.json()
+  else:
+    return {"error": "Unable to fetch data from the URL."}
+
+
+# To Get Chat ID and message which is sent by client
+def message_parser(message):
+  chat_id = message['message']['chat']['id']
+  if 'text' in message['message']:
+    text = message['message']['text']
+    print("Chat ID: ", chat_id)
+    print("Message: ", text)
+    return chat_id, text
+  else:
+    print("Chat ID: ", chat_id)
+    print("Message does not contain text.")
+    return chat_id, "Message does not contain text."
+
+
+# To send message using "SendMessage" API
+def send_message_telegram(chat_id, text):
+  url = f'https://api.telegram.org/bot{token}/sendMessage'
+  payload = {'chat_id': chat_id, 'text': text}
+  response = requests.post(url, json=payload)
+  return response
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+  if request.method == 'POST':
+    msg = request.get_json()
+    chat_id, incoming_que = message_parser(msg)
+    answer = generate_answer(incoming_que)
+    send_message_telegram(chat_id, answer)
+    return Response('ok', status=200)
+  else:
+    return "<h1>Something went wrong</h1>"
